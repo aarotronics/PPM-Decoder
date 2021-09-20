08f21b89224b012b18d3fdd2a4acdc5f47373830
aarotronics <aarotronics@gmail.com>
2021-09-20 16:25:11 +0200

    Updated PPM-Decoder.ino

--- PPM-Decoder.ino
+++ PPM-Decoder.ino
@@ -1,14 +1,17 @@
 /*
-                                          ARDUINO PPM DECODER FOR RC
-                                 ===============================================
    
-   This decoder uses external interrupts so pins D2 or D3 must be used in case of the ATMEGA328P. Make sure to 
-   adjust PPM_CHANNELS to the number of channels used by the system to decode.
-
-   "THE BEERWARE LICENSE" (Revision 42):
+                         ARDUINO PPM DECODER FOR RC
+              ================================================
+   
+   This decoder uses external interrupts so pins D2 or D3 must be used in 
+   case of the ATMEGA328P. Make sure to adjust PPM_CHANNELS to the number of 
+   channels used by the system to decode.
+   
+   This code is under "THE BEERWARE LICENSE" (Revision 42):
    Aaron G. wrote this code in October 2020. As long as you retain this
    notice, you can do whatever you want with this stuff. If we meet someday,
    and you think this stuff is worth it, you can buy me a beer in return.
+   
 */
 
 #define PPM_PIN           2
